2619be5b1b1477de11c2afe21b37a754d5aafe5e
Joshua Rosales Figueroa <joshuadrf@gmail.com>
2024-10-06 20:56:55 -0600

    Instalacion de Axios y modificacion de componentes

--- demo-reactjs/pages/RegisterUser/index.tsx
+++ demo-reactjs/pages/RegisterUser/index.tsx
@@ -1,21 +1,26 @@
-import React from "react";
-import { Button, Input, Space } from 'antd'; 
+import { Button, Form, Input, Space } from 'antd';
+import React from 'react';
+import { useDependencies } from './hooks';
 
 const RegisterUser = () => {
- return (
-    <>
-
-    <Space direction ='vertical'>
-        <Input placeholder='Nombre' />
-        <Input placeholder='Correo' />
-        <Input placeholder='Contraseña' type = 'password' />
-        <Button type='primary'>Registrar</Button>
-
-    </Space>
-
-    </>
-   ); 
-
-}; 
-
-export default RegisterUser; 
+	const { initialValues, handleSubmit, rules } = useDependencies();
+	return (
+		<Form onFinish={handleSubmit} initialValues={initialValues}>
+			<Space direction='vertical'>
+				<Form.Item name='name' rules={rules.name}>
+					<Input placeholder='Nombre' />
+				</Form.Item>
+				<Form.Item name='email' rules={rules.email}>
+					<Input placeholder='Correo' />
+				</Form.Item>
+				<Form.Item name='password' rules={rules.password}>
+					<Input placeholder='Contraseña' type='password' />
+				</Form.Item>
+				<Button type='primary' htmlType='submit'>
+					Registrar
+				</Button>
+			</Space>
+		</Form>
+	);
+};
+export default RegisterUser;
